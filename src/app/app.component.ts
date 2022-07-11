efd50c15c18999dce078845997580830262193a9
Hannah Ghassabeh <home@Hannahs-MacBook-Pro.local>
2022-07-10 21:44:29 -0400

    added dynamic updating of weekday buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,25 +11,37 @@ export class AppComponent implements OnInit {
   constructor(private weatherService: WeatherService){
     
   }
-
+  weekday1?: string;
+  weekday2?: string; 
+  weekday3?: string; 
+  weekday4?: string; 
+  weekday5?: string; 
   cityName: string = 'Toronto';
   weatherData?: WeatherData;
   
   ngOnInit(): void {
-    this.getWeatherData(this.cityName); 
+    this.getWeatherData(this.cityName);
   }
   onSubmit(){
     this.getWeatherData(this.cityName); 
     this.cityName = ''; 
-    console.log(this.getWeatherData(this.cityName)); 
+    console.log(this.getWeatherData(this.cityName));
   }
 
   private getWeatherData(cityName: string){
     this.weatherService.getWeatherData(cityName)
     .subscribe({
       next: (response: any) => {
-        this.weatherData = response; 
-        console.log(response);
+        this.weatherData = response;
+        if(this.weatherData)
+        {
+          let tempDate = new Date(this.weatherData.list[0].dt_txt);
+          this.weekday1= new Date(this.weatherData.list[0].dt_txt).toLocaleString('en-us', {  weekday: 'short' });
+          this.weekday2 = new Date(tempDate.setDate(tempDate.getDate()+1)).toLocaleString('en-us', {  weekday: 'short' });
+          this.weekday3 = new Date(tempDate.setDate(tempDate.getDate()+1)).toLocaleString('en-us', {  weekday: 'short' });
+          this.weekday4 = new Date(tempDate.setDate(tempDate.getDate()+1)).toLocaleString('en-us', {  weekday: 'short' });
+          this.weekday5 = new Date(tempDate.setDate(tempDate.getDate()+1)).toLocaleString('en-us', {  weekday: 'short' });
+        }
       }
     });
   }
