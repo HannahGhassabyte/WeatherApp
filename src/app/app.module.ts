5ae93e1095404c06b33c535a8638951a4e16d84f
Hannah Ghassabeh <home@Hannahs-MacBook-Pro.local>
2022-07-12 14:58:47 -0400

    instaniated mat-icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
 @NgModule({
   declarations: [
     AppComponent
@@ -14,6 +15,7 @@ import { FormsModule } from '@angular/forms';
     AppRoutingModule,
     HttpClientModule,
     MatButtonToggleModule,
+    MatIconModule,
     FormsModule
   ],
   providers: [],
